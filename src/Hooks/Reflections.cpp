18bd69dd303936c033d930da98bb71e493d9ebdf
CGray1234 <102427011+CGray1234@users.noreply.github.com>
2022-08-04 12:14:41 -0400

    Update Reflections.cpp

--- src/Hooks/Reflections.cpp
+++ src/Hooks/Reflections.cpp
@@ -1,13 +1,24 @@
-#include "main.hpp"
-
 #include "GlobalNamespace/MainSettingsModelSO.hpp"
 #include "GlobalNamespace/BoolSO.hpp"
 #include "GlobalNamespace/IntSO.hpp"
 
-#include "hooks.hpp"
+#include "UnityEngine/Application.hpp"
+
+#include "Includes.hpp"
+
+#include "ModConfig.hpp"
 
 MAKE_AUTO_HOOK_MATCH(Reflections, &GlobalNamespace::MainSettingsModelSO::Load, void, GlobalNamespace::MainSettingsModelSO* self, bool forced) {
-        self->mirrorGraphicsSettings->set_value(3);
+        
+        if (getModConfig().Reflections.GetValue() ==  true) {
+                self->mirrorGraphicsSettings->set_value(3);
 
-        Reflections(self, forced);
+                Reflections(self, forced);
+        }
+        else {
+                self->mirrorGraphicsSettings->set_value(0);
+
+                Reflections(self, forced);
+        }
+        
 }

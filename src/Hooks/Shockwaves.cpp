3aa7f441391f5f0b1f912bab916a60f8bb9ee795
CGray1234 <102427011+CGray1234@users.noreply.github.com>
2022-08-04 12:14:56 -0400

    Update Shockwaves.cpp

--- src/Hooks/Shockwaves.cpp
+++ src/Hooks/Shockwaves.cpp
@@ -1,14 +1,21 @@
-#include "main.hpp"
-
 #include "GlobalNamespace/MainSettingsModelSO.hpp"
 #include "GlobalNamespace/BoolSO.hpp"
 #include "GlobalNamespace/IntSO.hpp"
 
-#include "hooks.hpp"
+#include "Includes.hpp"
+
+#include "ModConfig.hpp"
 
 MAKE_AUTO_HOOK_MATCH(Shockwaves, &GlobalNamespace::MainSettingsModelSO::Load, void, GlobalNamespace::MainSettingsModelSO* self, bool forced) {
-        self->screenDisplacementEffectsEnabled->set_value(true);
-        self->maxShockwaveParticles->set_value(2);
 
-        Shockwaves(self, forced);
+        if (getModConfig().Shockwaves.GetValue() == true) {
+                self->screenDisplacementEffectsEnabled->set_value(true);
+
+                Shockwaves(self, forced);
+        }
+        else {
+                self->screenDisplacementEffectsEnabled->set_value(false);
+
+                Shockwaves(self, forced);
+        }
 }

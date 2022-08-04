a40ad72b37cd86a2ccb6beae4c3acab9c72cc495
CGray1234 <102427011+CGray1234@users.noreply.github.com>
2022-08-04 01:00:08 -0400

    Create ViewController.hpp

---
+++ include/UI/ViewController.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Includes.hpp"
+
+DECLARE_CLASS_CODEGEN(PCGraphics, pcgViewController, HMUI::ViewController,
+
+    DECLARE_OVERRIDE_METHOD(void, DidActivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidActivate", 3), bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+    DECLARE_OVERRIDE_METHOD(void, DidDeactivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidDeactivate", 2), bool removedFromHierarchy, bool screenSystemDisabling);
+)
